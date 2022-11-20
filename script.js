45fa56aeb26e1bf67e282c60fb07d4e49c3537b3
shoheimuto <believe.0409promise@gmail.com>
2022-11-20 20:22:00 +0900

    イベント発火 基礎編

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ window.addEventListener('load', function() {
     const pullDownButton = document.getElementById("lists");
 
     pullDownButton.addEventListener('mouseover', function() {
+        pullDownButton.setAttribute("style", "background-color:blue");
         console.log('乗った');
     
     })
     pullDownButton.addEventListener('click', function() {
+        pullDownButton.setAttribute("style", "background-color:green");
         console.log('クリック');
     
     })
     pullDownButton.addEventListener('mouseout', function() {
+        pullDownButton.removeAttribute("style", "background-color:red");
         console.log('離れた');
     
     })
