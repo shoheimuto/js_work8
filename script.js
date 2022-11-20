494882a60335b8d2c5824fce2f208f7e7f6ff309
shoheimuto <believe.0409promise@gmail.com>
2022-11-20 19:35:00 +0900

    イベント発火

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-let lists = document.getElementsByClassName("pull-down-list");
-console.log(lists);
+window.addEventListener('load', function() {
+    const pullDownButton = document.getElementById("lists");
+
+    pullDownButton.addEventListener('mouseover', function() {
+        console.log('乗った');
+    
+    })
+    pullDownButton.addEventListener('click', function() {
+        console.log('クリック');
+    
+    })
+    pullDownButton.addEventListener('mouseout', function() {
+        console.log('離れた');
+    
+    })
+})
+
